58e4f3dc51b68707613d6603edcf8bb9f2c16f09
n-jeremic <nikola.jeremic7@gmail.com>
2020-02-08 12:33:37 +0100

    Port variable updated

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
     console.log('Database connection is successful!');
   });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening for the requests on port ${port}...`);
 });
